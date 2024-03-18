21b3c2e3cb296f52298463e4e3ffb56a32cc7ece
Iris LC <iris.lazaro@enti.cat>
2024-03-18 13:25:49 +0100

    Finish matchmaking, do gameplay

--- Chess/Protocol.h
+++ Chess/Protocol.h
@@ -5,7 +5,14 @@
 
 //TODO: Crear fitxers per cada classe
 
-//PacketKey = 1
+/////////////////
+// MATCHMAKING //
+/////////////////
+
+//PacketKey: OnLogin = 0
+std::string name;
+
+//PacketKey: CreateRoomRequest = 1
 class CreateRoomRequest 
 {
 public:
@@ -13,7 +20,7 @@ public:
 };
 
 
-//PacketKey = 2
+//PacketKey: CreateRoomResponse = 2
 class CreateRoomResponse
 {
 public:
@@ -31,9 +38,112 @@ public:
 	__time32_t timestamp;		//Timestamp seed
 };
 
-//PacketKey = 3
+//PacketKey: RoomsUpdate = 3
 class RoomsUpdate
 {
 public:
 	std::vector<RoomData> rooms;
+};
+
+
+//PacketKey: JoinRoomPlayerRequest = 4
+unsigned long idRoom;
+
+class Address
+{
+	std::string ip;
+	unsigned short port;
+	std::string username;
+};
+
+//PacketKey: OnEnterAsPlayer = 5
+class OnEnterAsPlayer
+{
+	std::vector<Address> otherP2PClients;
+	bool isFirst;
+};
+
+//PacketKey: OnEnterAsPlayerFailure = 6 -> void (no té informació)
+
+
+//PacketKey: JoinRoomSpectatorRequest = 7
+unsigned long idRoom;
+
+
+//PacketKey: OnEnterAsSpectator = 8
+class OnEnterAsSpectator
+{
+	std::vector<Address> otherP2PClients;
+};
+
+
+//PacketKey: OnEnterAsSpectatorFailure = 9 -> void (no té informació)
+
+
+//PacketKey: ExitRoom = 10
+unsigned long idRoom;
+
+
+//PacketKey: SetReady = 11
+bool isReady;
+
+
+//PacketKey: GameStart = 12 -> void (no té informació)
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+//////////////
+// GAMEPLAY //
+//////////////
+
+class Vector2
+{
+	int x, y;		//Les coordenades de la peça
+};
+
+//PacketKey: SelectPiece = 0
+Vector2 selectedPosition;
+
+
+//PacketKey: DeselectKey = 1 -> void (no té informació)
+
+
+//PacketKey: MovePiece = 2
+Vector2 targetPosition;
+
+
+enum PieceType
+{
+	Pawn = 0, 
+	Knight = 1, 
+	Bishop = 2, 
+	Tower = 3, 
+	Queen = 4, 
+	King = 5
+};
+
+//PacketKey: Promotion = 3
+class Promotion
+{
+	Vector2 targetPosition;
+	PieceType piece;
+}; 
+
+
+//PacketKey: Castle = 4
+class Castle
+{
+	Vector2 kingTargetPosition;
+	Vector2 towerTargetPosition;
+	Vector2 towerCurrentPosition;
+};
+
+
+//PacketKey: EnPassant = 5
+class EnPassant
+{
+	Vector2 targetPosition;
+	Vector2 pawnKilledPosition;
 };
